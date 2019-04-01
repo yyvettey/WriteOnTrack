a5df3f14e1472ef5baa2d7183e278e0e6a944308
Daochen Zha <daochen.zha@tamu.edu>
2019-04-01 17:08:43 +0000

    Task delete bug fixed

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ApplicationRecord
 	belongs_to :user
 	has_many :timers , :dependent=> :destroy, :autosave => true
-	has_many :records , :dependent=> :destroy, :autosave => true
 	validates :current_value, numericality: {greater_than_or_equal_to: 0}
 	validates :target_value, presence: true, numericality: {greater_than: 0}
 	validates :target_date, numericality: {greater_than_or_equal_to: Time.now.to_i, message: "must be after today"}
